a6fe5f921f27c097561f7c558423d06398579ab7
Kelvi <Kelvi@KelvosPC.cpe.libertypr.com>
2017-02-08 08:29:00 -0400

    Uncommented line 6

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -3,7 +3,7 @@ import javax.swing.JFrame;
 public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Kelvin Garcia");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(200, 200);
 		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
