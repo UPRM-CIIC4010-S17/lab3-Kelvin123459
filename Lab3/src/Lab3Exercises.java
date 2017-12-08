918b4e828045acea014acdb8a657922b7680b65e
Kelvi <Kelvi@KelvosPC>
2017-12-08 15:59:17 -0400

    Changed name, size and location

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,9 +2,9 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Kelvin Garcia");
+		JFrame myFrame = new JFrame("Kelvin Garcia Muniz");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
+		myFrame.setLocationRelativeTo(null);
 		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
